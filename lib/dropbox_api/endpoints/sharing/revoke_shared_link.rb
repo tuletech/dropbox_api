e27e44601e489e2945ca62406aa38576e9095c82
Divyanshu Rathore <divyanshu03@live.com>
2019-02-13 14:28:11 +0530

    Adds /revoke_shared_link endpoint

---
+++ lib/dropbox_api/endpoints/sharing/revoke_shared_link.rb
@@ -0,0 +1,23 @@
+module DropboxApi::Endpoints::Sharing
+  class RevokeSharedLink < DropboxApi::Endpoints::Rpc
+    Method      = :post
+    Path        = "/2/sharing/revoke_shared_link".freeze
+    ResultType  = DropboxApi::Results::VoidResult
+    ErrorType   = DropboxApi::Errors::RevokeSharedLinkError
+
+    # Revoke a shared link.
+    #
+    # Note that even after revoking a shared link to a file, the file may be accessible
+    # if there are shared links leading to any of the file parent folders. 
+    #
+    # A successful response indicates that the shared link was revoked.
+    #
+    # @param url [String] shared url which needs to be revoked.
+
+    add_endpoint :revoke_shared_link do |url|
+      perform_request({
+        :url => url
+      })
+    end
+  end
+end

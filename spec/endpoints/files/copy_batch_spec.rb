e7d8a97f50a4f84d7cb2a5a9e3f5941d70f26a9e
Divyanshu Rathore <divyanshu03@live.com>
2019-02-19 19:29:08 +0530

    Fixes specs for older versions of ruby

--- spec/endpoints/files/copy_batch_spec.rb
+++ spec/endpoints/files/copy_batch_spec.rb
@@ -4,13 +4,13 @@ describe DropboxApi::Client, "#copy_batch" do
   end
 
   it "returns an async job id", :cassette => "copy_batch/success" do
-    result = @client.copy_batch [{"from_path": "/folder 1/photo1.png", "to_path": "/folder 2/photo1.png"}]
+    result = @client.copy_batch [{:from_path => "/folder 1/photo1.png", :to_path => "/folder 2/photo1.png"}]
     
     expect(result.async_job_id).to eq("dbjid:AABG1I-LPa5wH5dIqNL2_IY_LxQHTfdv_btYeCoTdOhGt-cUERJCj1xJkYWoxpX-jZau6_eCR2IjVJu3uhhLr9tr")
   end
 
   it "if autorename option is true, returns an async job id", :cassette => "copy_batch/autorename_success" do
-    result = @client.copy_batch([{"from_path": "/folder 2/photo1.png", "to_path": "/folder 3/photo1.png"}], autorename: true)
+    result = @client.copy_batch([{:from_path => "/folder 2/photo1.png", :to_path => "/folder 3/photo1.png"}], :autorename => true)
     
     expect(result.async_job_id).to eq("dbjid:AACFpNAiWBhdsxpppngzE8Ob3IJhYpFVJxzzBZmdLFwGwG2urEnxwfLrSDh2m1fYTxRYc6PllgijNUnxKuQZl920")
   end

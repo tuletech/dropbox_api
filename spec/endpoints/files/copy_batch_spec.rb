1c14939fc67feeaa4ccd9e9a528cc410ce8befed
Jesus Burgos Macia <jesus@visrez.com>
2019-03-23 09:46:22 +0100

    Adds scaffold generator to test copy_batch
    Related to #58.

--- spec/endpoints/files/copy_batch_spec.rb
+++ spec/endpoints/files/copy_batch_spec.rb
@@ -1,17 +1,26 @@
 describe DropboxApi::Client, "#copy_batch" do
+  let(:path_prefix) { DropboxScaffoldBuilder.prefix_for :copy_batch }
   before :each do
     @client = DropboxApi::Client.new
   end
 
   it "returns an async job id", :cassette => "copy_batch/success" do
-    result = @client.copy_batch [{:from_path => "/folder 1/photo1.png", :to_path => "/folder 2/photo1.png"}]
-    
-    expect(result.async_job_id).to eq("dbjid:AABG1I-LPa5wH5dIqNL2_IY_LxQHTfdv_btYeCoTdOhGt-cUERJCj1xJkYWoxpX-jZau6_eCR2IjVJu3uhhLr9tr")
+    result = @client.copy_batch [{
+      :from_path => "#{path_prefix}/regular_file.txt",
+      :to_path => "#{path_prefix}/regular_file_renamed.txt"
+    }]
+
+    expect(result.async_job_id)
+      .to eq("dbjid:AAA6b4uwc19nEau5k-OBI_h-hjrR7pNDaUA3_0hOwV-UZ2pkw_zXWp3FSuVZrQ0d9IXKGkwB5JdYI4mJZumLc6qZ")
   end
 
   it "if autorename option is true, returns an async job id", :cassette => "copy_batch/autorename_success" do
-    result = @client.copy_batch([{:from_path => "/folder 2/photo1.png", :to_path => "/folder 3/photo1.png"}], :autorename => true)
-    
-    expect(result.async_job_id).to eq("dbjid:AACFpNAiWBhdsxpppngzE8Ob3IJhYpFVJxzzBZmdLFwGwG2urEnxwfLrSDh2m1fYTxRYc6PllgijNUnxKuQZl920")
+    result = @client.copy_batch([{
+      :from_path => "#{path_prefix}/regular_file_2.txt",
+      :to_path => "#{path_prefix}/regular_file_2_renamed.txt"
+    }], :autorename => true)
+
+    expect(result.async_job_id)
+      .to eq("dbjid:AABe-bOUUq5nrybciMJxIxaGAD16nGCKByGyY3Z_2m6kshGW903XTikVT3_5V6JQQu20p3QxoCfws7_hT40deF6q")
   end
 end

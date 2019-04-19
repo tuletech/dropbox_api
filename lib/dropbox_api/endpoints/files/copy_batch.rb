02dbbd83b1dcce2df8a67424e0e8e954eb3d2993
Jesús Burgos Maciá <jburmac@gmail.com>
2019-04-19 09:08:25 +0200

    Improves documentation for `#copy_batch`

--- lib/dropbox_api/endpoints/files/copy_batch.rb
+++ lib/dropbox_api/endpoints/files/copy_batch.rb
@@ -6,19 +6,20 @@ module DropboxApi::Endpoints::Files
 
     include DropboxApi::OptionsValidator
 
-    # NOTE: No errors are returned by this endpoint.
-
     # Copy multiple files or folders to different locations at once in the
     # user's Dropbox.
-    # This route will either finish synchronously, or return a job ID and do
-    # the async copy job in background. Please use copy_batch_check to check
+    #
+    # This will either finish synchronously, or return a job ID and do
+    # the async copy job in background. Please use {#copy_batch_check} to check
     # the job status.
+    #
+    # Note: No errors are returned by this endpoint.
     #
     # @param entries [Array<Hash>] List of entries to be moved or copied.
     #   Each entry must be a hash with two keys: `:from_path` & `:to_path`.
     # @option options autorename [Boolean] If there's a conflict with any file,
     #   have the Dropbox server try to autorename that file to avoid the
-    #   conflict. The default for this field is False.
+    #   conflict. The default for this field is `false`.
     add_endpoint :copy_batch do |entries, options = {}|
       validate_options([
         :autorename

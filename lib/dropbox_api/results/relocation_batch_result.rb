dd68d14648dd0e0ead3356859b3dbb66e108330c
Jesús Burgos Maciá <jburmac@gmail.com>
2019-04-19 09:58:02 +0200

    Removes useless instance variable @entries
    Also updates the code style to remove some linter offenses.

--- lib/dropbox_api/results/relocation_batch_result.rb
+++ lib/dropbox_api/results/relocation_batch_result.rb
@@ -5,15 +5,16 @@ module DropboxApi::Results
       when "in_progress"
         :in_progress
       when "complete"
-        @entries ||= result_data["entries"].map do |entry|
-          if entry[".tag"] === "success"
+        result_data["entries"].map do |entry|
+          if entry[".tag"] == "success"
             DropboxApi::Metadata::Resource.new entry["success"]
           else
-            DropboxApi::Errors::RelocationBatchEntryError.build("#{entry["failure"][".tag"]}", entry["failure"])
+            DropboxApi::Errors::RelocationBatchEntryError
+              .build(entry["failure"][".tag"].to_s, entry["failure"])
           end
         end
       else
-        raise NotImplementedError, "Unknown result type: #{result_data[".tag"]}"
+        raise NotImplementedError, "Unknown result type: #{result_data['.tag']}"
       end
     end
   end

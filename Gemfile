cb0084a677462beaddec9294399c51820b499689
Jesús Burgos Maciá <jburmac@gmail.com>
2019-04-19 11:23:34 +0200

    Adds byebug for debugging

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gemspec
 
 group :development do
+  gem 'byebug'
+
   # Documentation
   gem 'redcarpet'
   gem 'yard-rspec', :git => "https://github.com/Jesus/yard-spec-plugin.git"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'dropbox_api'
 require 'support/vcr'
+require 'byebug'
 require File.expand_path('../support/dropbox_scaffold_builder', __FILE__)
 
 ENV["DROPBOX_OAUTH_BEARER"] ||= "MOCK_AUTHORIZATION_BEARER"

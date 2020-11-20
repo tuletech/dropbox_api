2166700efab70f54e5fd856f275f40f71d6f73f1
Wes Collatz <wes@tuletechnologies.com>
2020-11-20 15:27:02 -0800

    update v2 params

--- lib/dropbox_api/endpoints/files/search.rb
+++ lib/dropbox_api/endpoints/files/search.rb
@@ -1,8 +1,7 @@
 module DropboxApi::Endpoints::Files
   class Search < DropboxApi::Endpoints::Rpc
     Method      = :post
-    Path        = "/2/files/search".freeze
-    # Path        = "/2/files/search_v2".freeze
+    Path        = "/2/files/search_v2".freeze
     ResultType  = DropboxApi::Results::SearchResult
     ErrorType   = DropboxApi::Errors::SearchError
 
@@ -13,32 +12,23 @@ module DropboxApi::Endpoints::Files
     # Note: Recent changes may not immediately be reflected in search results
     # due to a short delay in indexing.
     #
-    # @param query [String] The string to search for. The search string is
-    #   split on spaces into multiple tokens. For file name searching, the last
-    #   token is used for prefix matching (i.e. "bat c" matches "bat cave" but
-    #   not "batman car").
-    # @param path [String] The path in the user's Dropbox to search.
-    # @option options start [Numeric] The starting index within the search
-    #   results (used for paging). The default for this field is 0.
-    # @option options max_results [Numeric] The maximum number of search
-    #   results to return. The default for this field is 100.
-    # @option options mode [:filename, :filename_and_content, :deleted_filename]
-    #   The search mode. Note that searching file content is only
-    #   available for Dropbox Business accounts. The default is filename.
-    add_endpoint :search do |query, path = "", options = {}|
-      validate_options([
-        :start,
-        :max_results,
-        :mode
-      ], options)
-      options[:start] ||= 0
-      options[:max_results] ||= 100
-      options[:mode] ||= :filename
 
-      perform_request options.merge({
-        :query => query,
-        :path => path
-      })
+    add_endpoint :search do |query, options = {}|
+      
+      validate_options([
+        :path,
+        :max_results,
+        :order_by,
+        :file_status,
+        :filename_only,
+        :file_categories,
+        :file_extension
+      ], options)
+
+      request_params = query
+      request_params[:options] = options unless options.empty?
+          
+      perform_request(request_params)
     end
   end
 end
